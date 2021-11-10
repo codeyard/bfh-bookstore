bd8599143be61b29f4386073949d6019dea7df93
Igor Stojanovic <i.stojanovic01@gmail.com>
2021-11-10 17:57:37 +0100

    init project setup

---
+++ book-orders/src/main/java/ch/rgis/bookorders/BookOrdersApplication.java
@@ -0,0 +1,13 @@
+package ch.rgis.bookorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookOrdersApplication.class, args);
+    }
+
+}
