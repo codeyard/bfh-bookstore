5c49b845eb912a92e08ad8a479857b1b23797f0c
Raphael Gerber <mail@raphaelgerber.com>
2021-11-10 20:09:55 +0100

    Implemented Customer and Payment classes

---
+++ book-orders/src/main/java/ch/rgis/bookorders/entity/Customer.java
@@ -0,0 +1,70 @@
+package ch.rgis.bookorders.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq")
+    @SequenceGenerator(name = "customer_seq", sequenceName = "customer_seq", initialValue = 10000, allocationSize = 50)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+
+    // <editor-fold desc="Getter and Setter">
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    // </editor-fold>
+}
---
+++ book-orders/src/main/java/ch/rgis/bookorders/entity/Payment.java
@@ -0,0 +1,66 @@
+package ch.rgis.bookorders.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payment_seq")
+    @SequenceGenerator(name = "payment_seq", sequenceName = "payment_seq", initialValue = 1000, allocationSize = 50)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "payment_date", nullable = false)
+    private LocalDateTime date;
+
+    private BigDecimal amount;
+    private String creditCardNumber;
+    private String transactionId;
+
+    // <editor-fold desc="Getter and Setter">
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    // </editor-fold>
+}
