6d3754418eb3aeb59d5a67d12190618d7a5ae194
Igor Stojanovic <i.stojanovic01@gmail.com>
2021-12-15 17:52:36 +0100

    setup shipping service

---
+++ book-orders/src/main/java/ch/rgis/bookorders/shipping/ShippingClient.java
@@ -0,0 +1,19 @@
+package ch.rgis.bookorders.shipping;
+
+public class ShippingClient {
+
+    public void sendShippingOrder() {
+
+    }
+
+    public void sendCancellation() {
+
+    }
+
+    public void receiveShippingInfo() {
+
+    }
+
+
+
+}
---
+++ book-shipping/src/main/java/com/example/bookshipping/BookShippingApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookshipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShippingApplication.class, args);
+	}
+
+}
